50e28aaf2eb9f3e0aab83ecc63284a31b8f93df4
leemcdaniel <lee.mcdaniel@gmail.com>
2015-09-28 11:27:54 -0500

    Added dweibull

--- R/testproj.R
+++ R/testproj.R
@@ -27,3 +27,19 @@ mweibull <- function(scale, shape){
   }
   return(integrate(func, 0, Inf)$value)
 }
+
+
+#' Weibull Density
+#' 
+#' @param t A vector of times
+#' @param scale Scale parameter
+#' @param shape Shape parameter
+#' @return The density
+#' @examples
+#' dweibull(1:10,1,3)
+#' @export
+dweibull <- function(t, scale, shape){
+  if(!all(t>0)) stop("Not all t > 0")
+  stopifnot(all(t>0), scale>0, shape>0)
+  return((shape/scale)*((t/scale)^(shape-1)*exp(-(t/scale)^shape)))
+}
