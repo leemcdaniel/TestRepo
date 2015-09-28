bfc654b609d7c795eaeba1fa5b1d581abd82db5d
leemcdaniel <lee.mcdaniel@gmail.com>
2015-09-28 11:12:15 -0500

    Initial Commit

---
+++ R/testproj.R
@@ -0,0 +1,29 @@
+#' Weibull Hazard Function
+#' 
+#' @param t A vector of times
+#' @param scale Scale parameter
+#' @param shape Shape parameter
+#' @return Value of the hazard function
+#' @examples
+#' hweibull(1:10, 5,3)
+#' @export
+hweibull <- function(t, scale, shape){
+  if(!all(t>0)) stop("Not all t > 0")
+  stopifnot(all(t>0), scale>0, shape>0)
+  return((shape/scale)*(t/scale)^(shape-1))
+}
+
+#' Weibull Mean
+#' 
+#' @param scale Scale parameter
+#' @param shape Shape parameter
+#' @return The mean
+#' @examples
+#' mweibull(1,3)
+#' @export
+mweibull <- function(scale, shape){
+  func <- function(t){
+    t*dweibull(t, scale=scale, shape=shape)
+  }
+  return(integrate(func, 0, Inf)$value)
+}
